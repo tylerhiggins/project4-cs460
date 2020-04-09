c7f43c4202730d9ecde8c97a47b7ffe0e29709f0
Jacob Pugsley <jacob.pugsley@wsu.edu>
2020-04-09 16:47:09 -0700

    created function to make backup directory

--- BackItUp.c
+++ BackItUp.c
@@ -5,14 +5,38 @@
 #include <string.h>
 #include <unistd.h>
 #include <semaphore.h>
+#include <sys/stat.h>
 
 #include "BackItUp.h"
 
 #define DEBUG 0
 
+void printError(char* error){
+	if( DEBUG ){
+		printf("Error: %s\n", error);
+	}
+}
+
+int createBackupDir(){
+	int err = mkdir("testdir/.backup", 0777);
+	if( err == -1 ){
+		if( errno == EEXIST ){
+			printf("Directory already exists.\n");
+			return 0;
+		}else{
+			printError(strerror(errno));
+		}
+	}
+	return 0;
+}
+
 
 int main(int argc, char **argv) {
 
+	createBackupDir();
 
 	return 0;
 }
+
+
+
